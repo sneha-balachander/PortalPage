2b4ff23c3791a638e80c97a14bd6baa40ecd079f
Sneha Balachander <sneha.balachander@bounteous.com>
2023-08-10 15:17:07 +0530

    Files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [ApiKey, setApiKey] = useState("");
+  const [isDragging, setIsDragging] = useState(false);
+  const [file, setFile] = useState([]);
+  const [extractedData, setExtractedData] = useState(false);
+
+  const handleSubmit = (e) => {};
+
+  const handleChange = (e) => {
+    setApiKey(e.target.value);
+  };
+
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    setIsDragging(false);
+  };
+
+  const handleDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    const droppedFiles = [...e.dataTransfer.files];
+    setFile(droppedFiles);
+  };
+
+  const handleFileChange = (e) => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="row">
+      <div className="col-6 container">
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <br />
+          <input
+            type="text"
+            value={ApiKey}
+            required
+            placeholder="Enter your API Key"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+          <br />
+          <input type="file" onChange={handleFileChange} />
+          <br />
+          <br />
+          <p className="text-center">OR</p>
+          <div
+            className={`file-drop-area ${isDragging ? "dragging" : ""}`}
+            onDragEnter={handleDragEnter}
+            onDragLeave={handleDragLeave}
+            onDragOver={handleDragOver}
+            onDrop={handleDrop}
+          >
+            {isDragging ? (
+              <p>Drop the files here</p>
+            ) : (
+              <p>Drag and drop files here</p>
+            )}
+
+            {file.map((file, index) => (
+              <p key={index}>{file.name}</p>
+            ))}
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      <div className="col-6 container">
+        {extractedData
+          ? " "
+          : "Upload your files here to get your important data!"}
+      </div>
     </div>
   );
 }
