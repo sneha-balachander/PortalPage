c63c0df7b1b7934ce96870ac8fac22d5f47ba0b5
Nikhil Anurag <nikhil.anurag@bounteous.com>
2023-08-11 15:53:58 +0530

    MergeConf Resolve

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/Navbar";
 import FileUpload from "./components/FileUpload";
-<<<<<<< HEAD
-import FileDownload from "./components/FileDownload";
-=======
-
->>>>>>> 28cbbe175bfad8742a5be34ebd856dcf94df9409
 function App() {
   const [checkFileUpload, setcheckFileUpload] = useState(false);
   const handleCheckFileUpload =(flag)=>{
